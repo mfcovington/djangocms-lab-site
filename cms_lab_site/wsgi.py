a8f02c52954b7816e472d02ed19eb04d0907fc1c
Mike Covington <mfcovington@gmail.com>
2015-04-19 00:18:20 -0700

    Set WSGI to use production settings and get secrets from the Apache environment

--- cms_lab_site/wsgi.py
+++ cms_lab_site/wsgi.py
@@ -7,8 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+from django.core.handlers.wsgi import WSGIHandler
+import django
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cms_lab_site.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+class WSGIEnvironment(WSGIHandler):
+
+    def __call__(self, environ, start_response):
+
+        os.environ['MALOOF_LAB_SECRET_KEY'] = environ['MALOOF_LAB_SECRET_KEY']
+        os.environ['MALOOF_LAB_SITE_DB_PASSWORD'] = environ['MALOOF_LAB_SITE_DB_PASSWORD']
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms_lab_site.settings.production')
+        django.setup()
+        return super(WSGIEnvironment, self).__call__(environ, start_response)
+
+application = WSGIEnvironment()
